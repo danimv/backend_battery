bbd6378762fc212f5005e87b59901521e794984c
Dani <danimv87@gmail.com>
2023-11-05 03:18:09 +0100

    sudo

--- install_servers_Linux.sh
+++ install_servers_Linux.sh
@@ -7,7 +7,7 @@ sudo apt install nodejs npm
 cd ../backend_battery
 
 # Start the Node.js backend server
-npm install
+sudo npm install
 # npm start &
 # Open a new terminal window and navigate to the React frontend directory
 # gnome-terminal --working-directory=/client -e "npm start"
@@ -15,8 +15,8 @@ npm install
 cd ../backend_battery/client
 
 # Start the React frontend server
-npm install
-npm run build
+sudo npm install
+sudo npm run build
 # npm start
 # npm install -g http-server
 sudo apt install nginx
